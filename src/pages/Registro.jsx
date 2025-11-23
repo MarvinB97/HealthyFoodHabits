bff55c47cd21bcb628cd36e5806ebfbd1b6c4a59
MarvinB <marvin.nunez@correounivalle.edu.co>
2025-11-23 10:46:43 -0500

    config

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -2,16 +2,33 @@ import { Button, Image } from "react-bootstrap";
 import AppBackground from "../components/AppBackground";
 import { useNavigate } from "react-router-dom";
 
+import { signInWithPopup } from "firebase/auth";
+import { auth, googleProvider } from "../firebase";
+
+
 const Registro = ()=>{
+    
     const navigate = useNavigate();
+    
+    const loginWithGoogle = async () => {
+        try {
+            const result = await signInWithPopup(auth, googleProvider);
+            console.log("Usuario logueado:", result.user);
+            navigate("/encuesta")
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    
     return(
         <>
             <AppBackground show={true}/>
             <div className="subContainer">
                 <p className="text-bold">¿Ya has usado la aplicación antes?</p>
                 <p>Continua con tu progreso, ingresando con tu cuenta de Google</p>
-                <Button className="btn-regular">
-                    Ingresar con Google <i class="fa-brands fa-google"></i>
+                <Button className="btn-regular" onClick={loginWithGoogle}>
+                    Ingresar con Google <i className="fa-brands fa-google"></i>
                 </Button>
                 <hr/>
                 <p className="text-bold">¿Eres  nuevo en la aplicación? ¡No hay problema!</p>
