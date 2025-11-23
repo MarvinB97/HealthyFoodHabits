3a7a5526c917414fbb013f65ff4c24b652413e05
MarvinB <marvin.nunez@correounivalle.edu.co>
2025-11-23 11:04:11 -0500

    logout

--- src/pages/Bienvenida.jsx
+++ src/pages/Bienvenida.jsx
@@ -1,9 +1,23 @@
 import { Button, Image } from "react-bootstrap";
 import AppBackground from "../components/AppBackground";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Bienvenida = ()=>{
     const navigate = useNavigate();
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+        });
+
+        return () => unsub();
+    }, []);
     
     return(
         <>
@@ -15,9 +29,17 @@ const Bienvenida = ()=>{
                 <p>Pequeños pasos hacia una alimentación mas consciente.</p>
                 <hr/>
                 <p>Registra tus comidas, descubre tu progreso y celebra cada elección saludable que hagas.</p>
+
+                {user ?
+                <Button className="btn-regular" onClick={() => navigate("/home")}>
+                    Comienza ahora
+                </Button>
+                :
                 <Button className="btn-regular" onClick={() => navigate("/registro")}>
                     Comienza ahora
                 </Button>
+                }
+                
             </div>
         </>
     );
