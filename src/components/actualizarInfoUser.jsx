79f3e6359b4ea0704c3466ff0ea936609b963974
paulrojasg <paulrodrigorojasecl@gmail.com>
2025-12-06 11:09:52 -0500

    Added points processing

--- src/components/actualizarInfoUser.jsx
+++ src/components/actualizarInfoUser.jsx
@@ -1,41 +1,39 @@
 import { auth } from "../firebase";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc } from "firebase/firestore";
 import { db } from "../firebase"; // importa tu instancia de Firestore
 
-
-
 export const actualizarInfoUser = async (codigo) => {
+  try {
+    const user = auth.currentUser; // usuario autenticado
+    console.log("Usuario autenticado:", user);
+
+    // Actualizar
     try {
-        
-        const user = auth.currentUser; // usuario autenticado
-        console.log("Usuario autenticado:", user);
+      const userRef = doc(db, "users", user.uid);
+      await setDoc(userRef, {
+        uid: user.uid,
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+        lastLogin: new Date(),
+        grupo: codigo,
+      });
 
-        // Actualizar
-        try {
-            await setDoc(doc(db, "users", user.uid), {
-                uid: user.uid,
-                name: user.displayName,
-                email: user.email,
-                photo: user.photoURL,
-                lastLogin: new Date(),
-                grupo: codigo
-            });
+      // 2. Agregar al usuario al grupo como miembro
+      await setDoc(doc(db, "grupos", codigo, "miembros", user.uid), {
+        uid: user.uid,
+        name: user.displayName,
+        fecha: new Date(),
+        userRef: userRef,
+      });
 
-            // 2. Agregar al usuario al grupo como miembro
-            await setDoc(doc(db, "grupos", codigo, "miembros", user.uid), {
-                uid: user.uid,
-                name: user.displayName,
-                fecha: new Date()
-            });
-
-            console.log("Usuario guardado con éxito");
-
-        } catch (err) {
-            console.error("Error Firestore:", err.code, err.message);
-        }
-
-        console.log("Usuario actualizado en Firestore");
-    } catch (error) { 
-        console.error(error);
+      console.log("Usuario guardado con éxito");
+    } catch (err) {
+      console.error("Error Firestore:", err.code, err.message);
     }
+
+    console.log("Usuario actualizado en Firestore");
+  } catch (error) {
+    console.error(error);
+  }
 };
